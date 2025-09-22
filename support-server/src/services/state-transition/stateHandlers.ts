6f359e6be5e0eea185f6626a1540aefd46fc2057
Matt Carter <96356887+0xDegenDeveloper@users.noreply.github.com>
2025-09-22 14:36:46 -0400

    fix conflict typo

--- support-server/src/services/state-transition/stateHandlers.ts
+++ support-server/src/services/state-transition/stateHandlers.ts
@@ -12,26 +12,22 @@ export class StateHandlers {
   private provider: RpcProvider;
   private account: Account;
 
-  constructor(
-    db: DB,
-    logger: Logger,
-    provider: RpcProvider,
-    account: Account,
-  ) {
+  constructor(db: DB, logger: Logger, provider: RpcProvider, account: Account) {
     this.db = db;
     this.logger = logger;
     this.provider = provider;
     this.account = account;
   }
 
-  async handleOpenState(roundContract: Contract, vaultContract: Contract, jobRequest: JobRequest|undefined) {
+  async handleOpenState(
+    roundContract: Contract,
+    vaultContract: Contract,
+    jobRequest: JobRequest | undefined,
+  ) {
     const ethAddress = await vaultContract.get_eth_address();
     const ethAddressHex = "0x" + BigInt(ethAddress).toString(16);
     const ethContract = new Contract(erc20ABI, ethAddressHex, this.account);
-    const data = await ethContract.transfer(
-      this.account.address,
-      123n
-    );
+    const data = await ethContract.transfer(this.account.address, 123n);
     console.log("DEBUGGING: data", data);
     await this.provider.waitForTransaction(data.transaction_hash);
     try {
@@ -42,20 +38,27 @@ export class StateHandlers {
         this.logger.info("First round detected - needs initialization");
         const requestData =
           await vaultContract.get_request_to_start_first_round();
-          console.log("DEBUGGING: jobRequest", jobRequest);
-          if (jobRequest?.status === JobStatus.Pending) {
-            this.logger.info(`Job request for vault ${vaultContract.address} is pending`);
-            return;
-          }
-          this.logger.info(`Job request for vault ${vaultContract.address} is failed, retrying`);
-          const response = await sendFossilRequest(
-            formatRawFossilRequest(requestData),
-            vaultContract,
-            this.logger
+        console.log("DEBUGGING: jobRequest", jobRequest);
+        if (jobRequest?.status === JobStatus.Pending) {
+          this.logger.info(
+            `Job request for vault ${vaultContract.address} is pending`,
           );
-          await this.db.upsertJobRequest(vaultContract.address, response.job_id, response.status as JobStatus);
           return;
-          
+        }
+        this.logger.info(
+          `Job request for vault ${vaultContract.address} is failed, retrying`,
+        );
+        const response = await sendFossilRequest(
+          formatRawFossilRequest(requestData),
+          vaultContract,
+          this.logger,
+        );
+        await this.db.upsertJobRequest(
+          vaultContract.address,
+          response.job_id,
+          response.status as JobStatus,
+        );
+        return;
 
         // The fossil request takes some time to process, so we'll exit here
         // and let the cron handle the state transition in the next iteration
@@ -64,7 +67,7 @@ export class StateHandlers {
       // Existing auction start logic
       //
       const auctionStartTime = Number(
-        await roundContract.get_auction_start_date()
+        await roundContract.get_auction_start_date(),
       );
 
       console.log("DEBUGGING: auctionStartTime", auctionStartTime);
@@ -73,11 +76,10 @@ export class StateHandlers {
         console.error("No latest block found");
         return;
       }
-      const latestStarknetBlock =
-        rpcToStarknetBlock(latestBlock);
+      const latestStarknetBlock = rpcToStarknetBlock(latestBlock);
       console.log(
         "DEBUGGING: latest starknet block timestamp" +
-          latestStarknetBlock.timestamp
+          latestStarknetBlock.timestamp,
       );
       console.log("DEBUGGING: now unix" + new Date().getTime() / 1000);
 
@@ -124,27 +126,26 @@ export class StateHandlers {
 
   async handleAuctioningState(
     roundContract: Contract,
-    vaultContract: Contract
+    vaultContract: Contract,
   ) {
     const auctionEndTimeRaw = await roundContract.get_auction_end_date();
     const auctionEndTime = Number(auctionEndTimeRaw);
 
-      const latestBlock = await this.provider.getBlock("latest");
-      if (!latestBlock) {
-        console.error("No latest block found");
-        return;
-      }
-      const latestStarknetBlock =
-        rpcToStarknetBlock(latestBlock);
-      if (latestStarknetBlock.timestamp < auctionEndTime) {
-        this.logger.info(
-          `Waiting for auction end time. Time left: ${formatTimeLeft(
-            latestStarknetBlock.timestamp,
-            auctionEndTime
-          )}`
-        );
-        return;
-      }
+    const latestBlock = await this.provider.getBlock("latest");
+    if (!latestBlock) {
+      console.error("No latest block found");
+      return;
+    }
+    const latestStarknetBlock = rpcToStarknetBlock(latestBlock);
+    if (latestStarknetBlock.timestamp < auctionEndTime) {
+      this.logger.info(
+        `Waiting for auction end time. Time left: ${formatTimeLeft(
+          latestStarknetBlock.timestamp,
+          auctionEndTime,
+        )}`,
+      );
+      return;
+    }
 
     this.logger.info("Ending auction...");
 
@@ -166,11 +167,11 @@ export class StateHandlers {
   async handleRunningState(
     roundContract: Contract,
     vaultContract: Contract,
-    jobRequest: JobRequest|undefined
+    jobRequest: JobRequest | undefined,
   ): Promise<void> {
     try {
       const settlementTime = Number(
-        await roundContract.get_option_settlement_date()
+        await roundContract.get_option_settlement_date(),
       );
 
       const latestBlock = await this.provider.getBlock("latest");
@@ -178,34 +179,29 @@ export class StateHandlers {
         console.error("No latest block found");
         return;
       }
-      const latestStarknetBlock =
-        rpcToStarknetBlock(latestBlock);
+      const latestStarknetBlock = rpcToStarknetBlock(latestBlock);
       if (latestStarknetBlock.timestamp < settlementTime) {
         this.logger.info(
           `Waiting for settlement time. Time left: ${formatTimeLeft(
             latestStarknetBlock.timestamp,
-            settlementTime
-          )}`
+            settlementTime,
+          )}`,
         );
         return;
       }
 
-    this.logger.info("Settlement time reached");
+      this.logger.info("Settlement time reached");
 
       if (jobRequest?.status === JobStatus.Pending) {
-        this.logger.info(`Job request for vault ${vaultContract.address} is pending`);
+        this.logger.info(
+          `Job request for vault ${vaultContract.address} is pending`,
+        );
         return;
       }
       const rawRequestData = await vaultContract.get_request_to_settle_round();
       const requestData = formatRawFossilRequest(rawRequestData);
 
-    const rawRequestData = await vaultContract.get_request_to_settle_round();
-
-      await sendFossilRequest(
-        requestData,
-        vaultContract,
-        this.logger
-      );
+      await sendFossilRequest(requestData, vaultContract, this.logger);
     } catch (error) {
       this.logger.error("Error handling Running state:", error);
       throw error;
