4a9b158b11f67f6a55beae1ef7b40699f1d0b418
Matt Carter <96356887+0xDegenDeveloper@users.noreply.github.com>
2025-09-23 14:42:54 -0400

    reduce event batch size for unconfirmed blocks

--- support-server/src/services/unconfirmed-twaps/runner.ts
+++ support-server/src/services/unconfirmed-twaps/runner.ts
@@ -55,7 +55,7 @@ export class UnconfirmedTWAPsRunner {
       let blockNumber = Number(lastProcessedBlock);
       while (blockNumber <= currentBlock) {
         try {
-          const length = Math.min(currentBlock - blockNumber + 1, 1000);
+          const length = Math.min(currentBlock - blockNumber + 1, 500);
           const blocks = await this.rpcClient.getBlocks(blockNumber, length);
           const shouldRecalibrate =
             await this.blockProcessor.processBlocks(blocks);
