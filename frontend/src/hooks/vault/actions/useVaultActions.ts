e8f93cbaffb0ade007d2fff224a6012ae2644855
Dhruv Gupta <dhruv035@gmail.com>
2025-10-09 12:56:11 +0530

    Bug fixes

--- frontend/src/hooks/vault/actions/useVaultActions.ts
+++ frontend/src/hooks/vault/actions/useVaultActions.ts
@@ -56,7 +56,7 @@ const useVaultActions = () => {
         const nonce = await provider?.getNonceForAddress(account?.address);
         const data = (
           argsData
-            ? await typedContract?.[functionName](...argsData, { nonce })
+            ? await typedContract?.[functionName](...argsData, { nonce, blockTag: "latest" })
             : await typedContract?.[functionName]()
         ) as TransactionResult;
 
