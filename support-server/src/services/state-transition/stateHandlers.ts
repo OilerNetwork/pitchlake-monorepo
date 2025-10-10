5491e20f7715d2ae75d4466873de1942c6631e18
Dhruv Gupta <dhruv035@gmail.com>
2025-10-10 13:16:11 +0530

    Hard code mock verifier always true

--- support-server/src/services/state-transition/stateHandlers.ts
+++ support-server/src/services/state-transition/stateHandlers.ts
@@ -20,7 +20,7 @@ export class StateHandlers {
   }
 
   private getRequestFunction() {
-    const useMockVerifier = process.env.USE_MOCK_VERIFIER;
+    const useMockVerifier = true;
     if (useMockVerifier) {
       return (fossilRequest: any, vaultContract: Contract, logger: any) => 
         sendMockFossilRequest(fossilRequest, vaultContract, logger, this.account);
